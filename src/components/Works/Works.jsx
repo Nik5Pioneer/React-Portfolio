1966b06e85cb7e5ba671200f1c5b9aee04387160
Nik2D <Cnik4321@gmail.com>
2023-09-18 23:07:39 +0545

    upload project

---
+++ src/components/Works/Works.jsx
@@ -0,0 +1,79 @@
+import React, { useContext } from "react";
+import "./Works.css";
+import Upwork from "../../img/Upwork.png";
+// import Wordpress from "../../img/Wordpress.png";
+import Figma from "../../img/Figma.png";
+import freelancer from "../../img/freelancer.png";
+import Facebook from "../../img/Facebook.png";
+import { themeContext } from "../../Context";
+import { motion } from "framer-motion";
+import {Link} from 'react-scroll'
+const Works = () => {
+  // context
+  const theme = useContext(themeContext);
+  const darkMode = theme.state.darkMode;
+
+  // transition
+  return (
+    <div className="works" id="works">
+      {/* left side */}
+      <div className="w-left">
+        <div className="awesome">
+          {/* dark Mode */}
+          <span style={{ color: darkMode ? "white" : "" }}>
+            Works for All these
+          </span>
+          <span>Brands & Clients</span>
+          <spane>
+            dummy text
+            <br />
+            dummy text
+            <br />
+            dummy text
+            <br />
+           dummy text
+          </spane>
+          <Link to="contact" smooth={true} spy={true}>
+            <button className="button s-button">Hire Me</button>
+          </Link>
+          <div
+            className="blur s-blur1"
+            style={{ background: "#ABF1FF94" }}
+          ></div>
+        </div>
+
+        {/* right side */}
+      </div>
+      <div className="w-right">
+        <motion.div
+          initial={{ rotate: 45 }}
+          whileInView={{ rotate: 0 }}
+          viewport={{ margin: "-40px" }}
+          transition={{ duration: 3.5, type: "spring" }}
+          className="w-mainCircle"
+        >
+          <div className="w-secCircle">
+            <img src={Upwork} alt="" />
+          </div>
+          {/* <div className="w-secCircle">
+            <img src={Wordpress} alt="" />
+          </div> */}
+          <div className="w-secCircle">
+            <img src={Figma} alt="" />
+          </div>{" "}
+          <div className="w-secCircle">
+            <img src={freelancer} alt="" />
+          </div>
+          <div className="w-secCircle">
+            <img src={Facebook} alt="" />
+          </div>
+        </motion.div>
+        {/* background Circles */}
+        <div className="w-backCircle blueCircle"></div>
+        <div className="w-backCircle yellowCircle"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Works;
