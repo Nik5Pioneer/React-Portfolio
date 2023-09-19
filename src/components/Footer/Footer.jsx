2301208127c381dea4d420199098993328b4db83
Nik2 <51456428+NikCdy@users.noreply.github.com>
2023-09-19 17:48:11 +0545

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -10,11 +10,11 @@ const Footer = () => {
     <div className="footer">
       <img src={Wave} alt="" style={{ width: "100%" }} />
       <div className="f-content">
-        <span>cnik4321@gmail.com</span>
+        <span>Cnik4321@gmail.com</span><span>.NikCdy</span>
         <div className="f-icons">
-          <Insta color="white" size={"3rem"} />
-          <Facebook color="white" size={"3rem"} />
-          <Gitub color="white" size={"3rem"} />
+          <a href="https://www.instagram.com/nik_cdy/"><Insta color="white" size={"3rem"} /></a>
+          <a href="https://www.facebook.com/shyam.chaudhary.7927/"><Facebook color="white" size={"3rem"} /></a>
+          <a href="https://github.com/NikCdy"><Gitub color="white" size={"3rem"} /></a>  
         </div>
       </div>
     </div>
